6acb7473e12931e26e838898bf4fc0fc2e76770f
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-01-15 10:21:30 +0100

    just download this files

--- index.php
+++ index.php
@@ -50,14 +50,94 @@
 
         // {var_dump() echo};
 
+        $value1 = " Learn PHP";
+
+        echo "<h2>" . $value1 . "</h2> . <br/>";
+
         
 
         $a = $b = $c = 'fruits';
+
+
+        print "<h2>PHP is Fun!</h2>";
+        print "Hello world!<br>";
+        print "I'm about to learn PHP!";
+
+        $isPlaying = true;
+        $notPlaying = false;
+
+        if($isPlaying){
+            echo "<br/> i am playing";
+        }elseif($notPlaying){
+            echo "i am not playing";
+        };
+
+        echo "<br/>";
+
+        $cars = array("BMW", "Nisan", "Benz", "Camery");
+        $cars = ["BMW", "Nisan", "Benz", "Camery"];
+        var_dump($cars );
+        echo "<br/>";
+
+        class Car {
+            public $color;
+            public $model;
+            public function __construct($color, $model) {
+              $this->color = $color;
+              $this->model = $model;
+            }
+            public function message() {
+              return "My car is a " . $this->color . " " . $this->model . "!";
+            }
+          }
+          
+          $myCar = new Car("red", "Volvo");
+          var_dump($myCar);
+
+          echo "<br/>";
+
+
+
+          class Person {
+            public $Name;
+            public $Age;
+            public $Lastname;
+        
+            public function __construct($Name, $Age, $Lastname){
+                $this->Name = $Name;
+                $this->Age = $Age;
+                $this->Lastname = $Lastname;
+            }
+        
+            public function PrintMessage() {
+                return "My Name is " . $this->Name . " " . $this->Lastname . " and Age is " . $this->Age;
+            }
+        }
+        
+        $Brother = new Person("John", 50, "Doe");
+        var_dump($Brother);
+        
+        $empty = NULL;
+        $empty = "emmanuel: ";
+        var_dump($empty);
+
+        echo "<br/>";
+        
+        $studentAge = 10;
+        
+        $studentAge = (string) $studentAge;
+        var_dump($studentAge);
+        
+        echo "<br/>";
+
+        echo strlen("Ekener");
+
+        str_word_count()
+        echo str_word_count("Hello world!");
+
         ?>
 
-<script>
-            document.write($b);
-        </script>
+
         
 </body>
 </html>
